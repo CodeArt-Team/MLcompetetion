ef7ab2cf34de9177a073915183a632747009a27d
shin-na-ra <nas221@naver.com>
2024-09-10 13:35:13 +0900

    시간, 역 설정 화면 분리

--- 공모전/(3)GovTech_MetroComplex_WS/Harusijack_v.4.1.1/HaruSijak/View/Setting/SettingView.swift
+++ 공모전/(3)GovTech_MetroComplex_WS/Harusijack_v.4.1.1/HaruSijak/View/Setting/SettingView.swift
@@ -20,42 +20,102 @@
         * 2024.06.27 by pdg : 시간 설정 칸 이랑 버전 정보랑 구분이 안감. -> 시간 설정은 버튼화 했으면 좋겠음.
         * 2024.08.19 by snr : 역, 호선, 시간 변경 시 바로 update되어 조회되도록 수정
         * 2024.09.09 by snr : 역, 시간 정보를 설정했을 때만 상세정보가 보이도록 수정
+        * 2024.09.10 by snr : 뒤로가기 버튼 커스터마이징.
  
  */
 import SwiftUI
 struct SettingView: View {
-    @State var isShowSheet: Bool = false    // 시간 변경 sheet alert
-    let dbModel = TimeSettingDB()           // 시간 설정 vm instance 생성
-    @State var infoList: [Time] = []        // 조회된 시간정보 담을 리스트
+    @State var isShowSheet: Bool = false        // 시간 변경 sheet alert
+    @State var isStationSheet: Bool = false     // 역 변경 sheet alert
+    let dbModel = TimeSettingDB()               // 시간 설정 vm instance 생성
+    @State var infoList: [Time] = []            // 조회된 시간정보 담을 리스트
     
     @State var stationName: String =  TimeSettingDB().queryDB().first!.station
     @State var line: Int = TimeSettingDB().queryDB().first!.line
     @State var time: Int = TimeSettingDB().queryDB().first!.time
     
+    @Environment(\.dismiss) var dismiss;
+    
 
     var body: some View {
             VStack(content: {
-                //출근 시간대 설정하기
+                
+                // ********* 백업 *********
+//                HStack(content: {
+//                    Button(action: {
+//                        isShowSheet = true
+//                    }, label: {
+//                        Image(systemName: "slider.horizontal.2.square")
+//                            .font(.system(size: 25))
+//                            .padding()
+//
+//                            Text("출발역 지정하기")
+//
+//                            // line이 124가 아닐 때만, 즉 시간과 역설정 되었을 때만 해당 정보가 출력되도록 함.
+//                            if line != 124{
+//                                Text("(\(stationName)역 [\(line)호선],  \(time)시)") // 저장되어있는 설정시간 보여주기
+//                                    .font(.custom("Ownglyph_noocar-Rg", size: 18))
+//                                    .foregroundStyle(.gray)
+//                                    .padding(.top, 3)
+//                            }
+//                    })
+//
+//                })
+//                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+//                .padding(.leading, 20)
+//                .onTapGesture {
+//                    isShowSheet = true
+//                }
+                
+            // ********* 출근 역 설정버튼 start *********
+                HStack(content: {
+                    Button(action: {
+                        isStationSheet = true
+                    }, label: {
+                        Image(systemName: "train.side.rear.car")
+                            .font(.system(size: 25))
+                            .padding()
+                        
+                        Text("출발역")
+                            .padding(.trailing, 20)
+                        
+                        // line이 124가 아닐 때만, 즉 시간과 역설정 되었을 때만 해당 정보가 출력되도록 함.
+                        if line != 124{
+                            Text("\(stationName)역 [\(line)호선]") // 저장되어있는 설정시간 보여주기
+                                .font(.custom("Ownglyph_noocar-Rg", size: 20))
+                                .foregroundStyle(.gray)
+                                .padding(.top, 3)
+                        }
+                    })
+                    
+                })
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                .padding(.leading, 20)
+                .onTapGesture {
+                    isStationSheet = true
+                }
+                
+                
+            
+                // ********* 시간대 설정버튼 start *********
                 HStack(content: {
                     Button(action: {
                         isShowSheet = true
                     }, label: {
-                        Image(systemName: "slider.horizontal.2.square")
+                        Image(systemName: "clock")
                             .font(.system(size: 25))
                             .padding()
                         
-//                        VStack(alignment: .leading, content: {
-                            Text("역, 시간 설정하기")
+                            Text("출발시간")
+                                .padding(.trailing, 15)
                             
                             // line이 124가 아닐 때만, 즉 시간과 역설정 되었을 때만 해당 정보가 출력되도록 함.
                             if line != 124{
-                                Text("(\(stationName)역 [\(line)호선],  \(time)시)") // 저장되어있는 설정시간 보여주기
-                                    .font(.custom("Ownglyph_noocar-Rg", size: 18))
+                                Text("\(time)시") // 저장되어있는 설정시간 보여주기
+                                    .font(.custom("Ownglyph_noocar-Rg", size: 20))
                                     .foregroundStyle(.gray)
                                     .padding(.top, 3)
                             }
-                            
-//                        })
                     })
                     
                 })
@@ -66,7 +126,9 @@ struct SettingView: View {
                 }
                 
                 
-                // 버전정보 출력
+                
+                
+                // ********* 버전정보 start  *********
                 HStack(content: {
                     Image(systemName: "v.square")
                         .font(.system(size: 25))
@@ -77,25 +139,47 @@ struct SettingView: View {
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
                 .padding(.leading, 20)
                 
+                
                 Spacer()
                 
             })//Vstack
             .padding(.top, 30)
             .font(.custom("Ownglyph_noocar-Rg", size: 20))
             .sheet(isPresented: $isShowSheet, content: {
-                TimeSettingView(titleName: "역, 시간 설정하기", stationValue: $stationName, lineValue: $line, timeValue: $time)
+                TimeSettingView(titleName: "출발시간 설정", stationValue: $stationName, lineValue: $line, timeValue: $time)
                     .onDisappear{
-                        fetchTasksForSelectedDate()
+//                        fetchTasksForSelectedDate()
+                        print("Updated TimeValue: \(stationName), line: \(line), time: \(time)")
                     }
                     .presentationDragIndicator(.visible)
                     .presentationDetents([.medium])
-            })//sheet
+            })// 시간 설정 sheet
+            .sheet(isPresented: $isStationSheet, content: {
+                StationSettingView(titleName: "출발역 설정", stationValue: $stationName, lineValue: $line, timeValue: $time)
+                    .onDisappear{
+                        fetchTasksForSelectedDate()
+                      print("Updated stationValue: \(stationName), line: \(line), time: \(time)")
+                    }
+                    .presentationDragIndicator(.visible)
+                    .presentationDetents([.medium])
+            }) // 역 설정 sheet
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     Text("설정")
                         .font(.custom("Tenada", size: 30))
                 }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button(action: {
+                        dismiss() //뒤로가기 함수
+                    }, label: {
+                        Image(systemName: "house.fill")
+                            .imageScale(.large)
+                            .foregroundColor(.primary)
+                    })
+                }
             }
+            .navigationBarBackButtonHidden(true) //Back 버튼 숨기기
         }
     
     
@@ -112,6 +196,6 @@ struct SettingView: View {
     }
 }
 
-//#Preview {
-//    SettingView(stationName: "가디", line: 1, time: 1)
-//}
+#Preview {
+    SettingView(stationName: "가디", line: 1, time: 1)
+}
--- 공모전/(3)GovTech_MetroComplex_WS/Harusijack_v.4.1.1/HaruSijak/View/Setting/StationSettingView.swift
+++ 공모전/(3)GovTech_MetroComplex_WS/Harusijack_v.4.1.1/HaruSijak/View/Setting/StationSettingView.swift
@@ -1,26 +1,21 @@
 //
-//  TimeSettingView.swift
+//  StationSettingView.swift
 //  HaruSijak
 //
-//  Created by 신나라 on 6/13/24.
+//  Created by 신나라 on 9/10/24.
 /*
     Description : HaruSijack App 개발 setting page
-    Date : 2024.6.13
+    Date : 2024.09.10
     Author : snr
     Detail :
     Updates :
-        * 2024.07.05 by snr : 호선과 역명 설정되도록 수정
-        * 2024.09.09 by snr : 
-            1. 제목, 버튼 글꼴 수정
-            2. SettingView에서 넘어오는 Line 값이 임의로 설정한 124일 경우, 1호선부터 조회되도록 selectedLine = 0 처리
         * 2024.09.10 by snr : 시간, 역 설정 화면 분리
  */
 
 import SwiftUI
 
-struct TimeSettingView: View {
+struct StationSettingView: View {
     
-//    @State var isShowSheet: Bool = false    // 시간 변경 sheet alert
     let timeList = [Int](5..<25)            // 5~24까지 리스트
     let line23 = SubwayList().totalStation
     
@@ -59,24 +54,58 @@ struct TimeSettingView: View {
                     .padding(.top, 20)
                 
                 
-                // ---------------- time picker -------------------
-                Picker("", selection: $selectedTime, content: {
-                    ForEach(0..<timeList.count, id:\.self, content: { index in
-                        Text("\(timeList[index])시").tag(index)
-                            .font(.custom("Ownglyph_noocar-Rg", size: 30))
+                
+                // ---------------- line picker -------------------
+                HStack {
+                    Text("호선 선택 : ")
+                        .font(.custom("Ownglyph_noocar-Rg", size: 20))
+                    
+                    Picker("", selection: $selectedLine, content: {
+                        ForEach(0..<line.count, id:\.self, content: { index in
+                            Text("\(line.map{$0}.sorted()[index])호선").tag(index)
+                                .font(.custom("Ownglyph_noocar-Rg", size: 30))
+                        })
                     })
-                })
-                .pickerStyle(.wheel)
+                    .pickerStyle(.menu)
+                }
+                .padding(.top, 40)
+//                .onAppear {
+//                    // 초기 값 설정
+//                    selectedStation = filteredStatioins.firstIndex(where: { $0.0 == stationValue }) ?? 0
+//                    selectedLine = lineValue
+//                }
+                
+                
+                // ---------------- station picker -------------------
+                HStack {
+                    Text("역 선택 : ")
+                        .font(.custom("Ownglyph_noocar-Rg", size: 20))
+                    
+                    Picker("", selection: $selectedStation, content: {
+                        ForEach(filteredStatioins.indices, id: \.self) { index in
+                            Text(self.filteredStatioins[index].0).tag(index)
+                        }
+                        .pickerStyle(MenuPickerStyle())
+                    })
+                    .pickerStyle(.menu)
+                }
+                .padding(.top, 10)
+                
                 
                 // ---------------- 변경 button -------------------
                 Button(buttonTitle, action: {
                     infoList = dbModel.queryDB()
                     
+                    print("infoList.count : ",infoList.count)
+                    
                     // 시간 설정 정보가 없을 때 => insert
                     if infoList.isEmpty {
+                        print("infoList is empty, setting isAdd to true")
                         alertType = .add
                         
                     } else {
+                        // update 처리
+                        print("infoList is not empty, setting isUpdate to true")
                         alertType = .update
                     }
                 }) // Button
@@ -97,11 +126,10 @@ struct TimeSettingView: View {
                     case .add:
                         return Alert(
                             title: Text("알림"),
-                            message: Text("\(timeList[selectedTime])시로 \n 설정하시겠습니까?"),
+                            message: Text("\(filteredStatioins[selectedStation].0)역으로 \n 설정하시겠습니까?"),
                             primaryButton: .destructive(Text("확인"), action: {
                                 dbModel.insertDB(time: timeList[selectedTime], station: filteredStatioins[selectedStation].0, line: line[selectedLine])
                                 
-                                // * Binding으로 전달된 변수 갱신
                                 stationValue = filteredStatioins[selectedStation].0
                                 lineValue = line[selectedLine]
                                 timeValue = timeList[selectedTime]
@@ -113,15 +141,14 @@ struct TimeSettingView: View {
                     case .update:
                         return Alert(
                             title: Text("알림"),
-                            message: Text("\(timeList[selectedTime])시로 \n 설정하시겠습니까?"),
+                            message: Text("\(filteredStatioins[selectedStation].0)역으로 \n 설정하시겠습니까?"),
                             primaryButton: .destructive(Text("확인"), action: {
                                 dbModel.updateDB(time: timeList[selectedTime], station: filteredStatioins[selectedStation].0, line: line[selectedLine], id: infoList[0].id)
-                                
-                                // * Binding으로 전달된 변수 갱신
+                               
                                 stationValue = filteredStatioins[selectedStation].0
                                 lineValue = line[selectedLine]
                                 timeValue = timeList[selectedTime]
-
+                                
                                 dismiss()
                             }),
                             secondaryButton: .cancel(Text("취소"))
@@ -148,14 +175,7 @@ struct TimeSettingView: View {
     }
 }
 
-enum SettingAlertType: Identifiable {
-    case add
-    case update
-    
-    var id: Int{
-        hashValue
-    }
-}
 #Preview {
-    TimeSettingView(titleName: "테스트제목", stationValue: .constant("Initial Station Value"), lineValue: .constant(1), timeValue: .constant(1))
+    StationSettingView(titleName: "테스트제목", stationValue: .constant("Initial Station Value"), lineValue: .constant(1), timeValue: .constant(1))
 }
+
